40e4e3866f66805f6d032c021801e8a5987d1689
dev universal <dev@capitallegacy.co.za>
2024-06-07 08:41:52 +0200

    fix

--- resources/views/class/header.blade.php
+++ resources/views/class/header.blade.php
@@ -14,7 +14,7 @@ class {{ $class['name'] }} extends Base
 @foreach ($class['constants'] as $constant => $value)
 @if(is_string($value))
     const {{ $constant }} = '{{ $value }}';
-@elseif(!is_bool($value))
+@elseif(is_bool($value))
     const {{ $constant }} = {{ $value ? 'true' : 'false' }};
 @elseif(!is_array($value))
     const {{ $constant }} = {{ $value }};
